c7691413476bb12c11104ac325797634b370e97d
AMCA15 <38790021+AMCA15@users.noreply.github.com>
2019-04-06 01:30:59 -0400

    Refactoring. Improve axis for time domain plotting

--- Lab 1/ModulationModel.m
+++ Lab 1/ModulationModel.m
@@ -27,8 +27,8 @@ classdef ModulationModel < handle
        f_lpf
     end
     properties (SetAccess = private, Hidden)
-       t                     % Escala de tiempo
-       F                     % Escala de frecuencia
+       t           % Escala de tiempo
+       F           % Escala de frecuencia
     end
     
     methods
@@ -98,7 +98,6 @@ classdef ModulationModel < handle
             obj.y_LPF = lowpass(obj.y_D, obj.f_lpf, obj.Fs);
         end
         
-       
         function dispPower(obj)
             % Message's power
             power_msg = round(rms(obj.msg)^2, 3, 'decimals');
@@ -114,91 +113,85 @@ classdef ModulationModel < handle
             % Banda Pass Filter's power
             power_signal_BPF = round(rms(obj.y_BPF)^2, 3, 'decimals');
             power_noise_BPF = round(rms(obj.y_BPF - y_BPF_no_noise)^2, 3, 'decimals');
-            s_n_BPF = round(power_signal_BPF / power_noise_BPF, 2, 'decimals');
             
             % Low Pass Filter's power
             power_signal_LPF = round(rms(obj.y_LPF)^2, 3, 'decimals');
             power_noise_LPF = round(rms(obj.y_LPF - y_LPF_no_noise)^2, 3, 'decimals');
-            s_n_LPF = round(power_signal_LPF / power_noise_LPF, 2, 'decimals');
-            
+              
+            obj.PrintPwrMsgs(power_msg, power_msg_mod);
+            obj.PrintPwrSignalNoise("A la salida del filtro pasabanda", power_signal_BPF, power_noise_BPF);
+            obj.PrintPwrSignalNoise("A la salida del filtro pasabajo", power_signal_LPF, power_noise_LPF);
+        end
+        
+        % Plot time domain and frequency domain
+        function plot(obj)
+            obj.plot_layout('Mensaje Original', 2, 1, 'Vertical', obj.msg, "");
+            obj.plot_layout('Mensaje Modulado', 2, 1, 'Vertical', obj.msg_mod, "");
+            obj.plot_layout('Mensaje en el Canal', 2, 1, 'Vertical', obj.msg_canal, "");
+            obj.plot_layout('Mensaje en el Receptor', 3, 2, 'Horizontal', [obj.y_BPF; obj.y_D; obj.y_LPF], [": Pasa Banda"; ": Detector"; ": Pasa Bajo"]);
+        end      
+    end
+    
+    methods (Hidden)
+        function PrintPwrMsgs(obj, power_msg, power_msg_mod)
             disp("======================================");
             disp("Potencia del mensaje: " + power_msg);
             disp("======================================");
             disp("A la salida del transmisor");
             disp("Potencia de la señal: " + power_msg_mod);
+        end
+        function PrintPwrSignalNoise(obj, title, power_signal, power_noise)
+            sn = round(power_signal / power_noise, 2, 'decimals');
             disp("======================================");
-            disp("A la salida del filtro pasabanda");
-            disp("Potencia de la señal: " + power_signal_BPF);
-            disp("Potencia del ruido: " + power_noise_BPF);
-            disp("Relacion señal a ruido: " + s_n_BPF + " (" + round(mag2db(s_n_BPF), 2, 'decimals') + " dB)");
-            disp("======================================");
-            disp("A la salida del filtro pasabajo");
-            disp("Potencia de la señal: " + power_signal_LPF);
-            disp("Potencia del ruido: " + power_noise_LPF);
-            disp("Relacion señal a ruido: " + s_n_LPF + " (" + round(mag2db(s_n_LPF), 2, 'decimals') + " dB)");
+            disp(title);
+            disp("Potencia de la señal: " + power_signal);
+            disp("Potencia del ruido: " + power_noise);
+            disp("Relacion señal a ruido: " + sn + " (" + round(mag2db(sn), 2, 'decimals') + " dB)");
         end
         
-        function plot(obj)
-            obj.plot_template1('Mensaje Original', obj.msg);
-            obj.plot_template1('Mensaje Modulado', obj.msg_mod);
-            obj.plot_template1('Mensaje en el Canal', obj.msg_canal);
-            obj.plot_template2('Mensaje en el Receptor', 2, 3, [obj.y_BPF, obj.y_D, obj.y_LPF], ['Pasa Banda', 'Detector', 'Pasa Bajo']);
-        end      
-        
-        function plot_template1(obj, Title, Signal)
+        function plot_layout(obj, Title, Rows, Columns, Orientation, Signals, SignalsName)
             figure('Name', Title);
-            % Graficar señal en tiempo
-            subplot(2,1,1);
-            plot(obj.t, Signal);
-            axis([0 0.025 -1 1]);
-            obj.plot_labels_time();
+            
+            if Orientation == "Horizontal"
+               Index = Rows; 
+            else
+               Index = Columns;
+            end
 
-            % Graficar espectro de la señal
-            subplot(2,1,2);
-            M=abs(fftshift(fft(Signal)));
-            M=M/obj.N;
-            plot(obj.F,M);
-            axis([-3000 3000 0 0.5])
-            plot_labels_frecuency(obj)
-            grid on 
-        end
-        
-        function plot_template2(obj, Title, Rows, Columns, Signals, SignalsName)
-            figure('Name', Title);
-            % Graficar señal en tiempo
-            for i = 1:Rows*Columns - 1
-                subplot(Rows,Columns,i);
-                plot(obj.t, Signals(i));
-                obj.plot_labels_time();
-                title('Señal en el tiempo: ' + SignalsName(i));
-                axis([0 0.01 -1 1])    
+            for i = 1:Index
+                % Graficar señal en tiempo
+                subplot(Rows,Columns,(i*2-1));
+                plot(obj.t, Signals(i,:));
+                obj.plot_labels_time(SignalsName(i));
+                % Plot only 4 periods
+                [~, locs] = findpeaks(obj.msg, obj.Fs);
+                xUpperLim = mean(diff(locs))*4;
+                axis([0 xUpperLim -inf inf])   
                 
-                
-                subplot(Rows,Columns,i+1);
-                M=abs(fftshift(fft(Signals(i))));
+                % Graficar señal en frecuencia
+                subplot(Rows,Columns,i*2);
+                M=abs(fftshift(fft(Signals(i,:))));
                 M=M/obj.N;
                 plot(obj.F,M);
-                obj.plot_labels_frecuency()
-                title('Espectro de Frecuencia: ' + SignalsName(i));
-                axis([-15000 15000 0 0.5])
+                obj.plot_labels_frecuency(SignalsName(i))
+                axis([-15000 15000 -inf inf])
             end
         end
         
         % Agrega etiquetas de graficas de tiempo
-        function plot_labels_time(obj)
-            title('Señal en el tiempo');
+        function plot_labels_time(obj, SignalsName)
+            title('Señal en el tiempo' + SignalsName);
             xlabel('Tiempo (s)');
             ylabel('Msg Mod (t)');
             grid on;
         end
 
         % Agrega etiquetas en graficas de frecuencia
-        function plot_labels_frecuency(obj)
-            title('Espectro de Frecuencia');
+        function plot_labels_frecuency(obj, SignalsName)
+            title('Espectro de Frecuencia' + SignalsName);
             xlabel('Frecuencia (Hz)');
             ylabel('Msg (F)');
             grid on;
         end
-        
     end
 end
